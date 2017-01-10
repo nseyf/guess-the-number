4d3c3edd8d80e9add62d84fbc848544b5bcf9e4a
Nima Seyf <nimaseyf@gmail.com>
2017-01-10 12:16:59 +0000

    fixed reset button

--- js/index.js
+++ js/index.js
@@ -1,52 +1,63 @@
 'use strict';
 
 var number = Math.floor(Math.random() * 50) + 1;
+var randomNumberArray = [];
 
-function analyseGuess() {
+randomNumberArray.push(number);
 
-  var guess = document.getElementById('guess').value;
+function analyseGuess(){
+
+   var guess = document.getElementById('guess').value;
 
   var tips = document.getElementById('tips');
 
-  if (guess == number) {
+ if(guess == randomNumberArray.slice(-1)[0]){
 
-    var correctGuess = document.createElement('li');
-    var numberOfGuesses = document.getElementsByTagName('li').length;
+   var correctGuess = document.createElement('li');
+   var numberOfGuesses = document.getElementsByTagName('li').length;
 
-    document.getElementById('submit').disabled = true;
+  document.getElementById('submit').disabled = true;
 
-    if (numberOfGuesses > 0) {
-      correctGuess.appendChild(document.createTextNode("That's it! it was " + number + ". Not bad, that only took " + numberOfGuesses + " guesses."));
+ if(numberOfGuesses > 0){  correctGuess.appendChild(document.createTextNode("That's it! it was " + guess + ". Not bad, that only took " + numberOfGuesses + " guesses."));
 
-      tips.appendChild(correctGuess);
-    } else {
-      correctGuess.appendChild(document.createTextNode("Wow! you guessed " + number + " correctly the first time!"));
-      tips.appendChild(correctGuess);
-    }
-  } else if (guess > 50) {
-    var outofBounds = document.createElement('li');
+   tips.appendChild(correctGuess);
+                        } else {
+                          correctGuess.appendChild(document.createTextNode("Wow! you guessed " + guess + " correctly the first time!"));
+                          tips.appendChild(correctGuess);
+                        }
 
-    outofBounds.appendChild(document.createTextNode(guess + "? It's a number between 1 and 50!"));
 
-    tips.appendChild(outofBounds);
-  } else if (guess > number) {
+ }  else if (guess > 50){
+   var outofBounds = document.createElement('li');
 
-    var tooHigh = document.createElement('li');
+   outofBounds.appendChild(document.createTextNode(guess + "? It's a number between 1 and 50!"));
 
-    tooHigh.appendChild(document.createTextNode("Nope, " + guess + "'s too high. Try Again!"));
+   tips.appendChild(outofBounds);
 
-    tips.appendChild(tooHigh);
-  } else if (guess < number) {
+} else if (guess > randomNumberArray.slice(-1)[0]) {
 
-    var tooLow = document.createElement('li');
+   var tooHigh = document.createElement('li');
 
-    tooLow.appendChild(document.createTextNode("Nope, " + guess + "'s too low. Try Again!"));
+   tooHigh.appendChild(document.createTextNode("Nope, " + guess + "'s too high. Try Again!"));
 
-    tips.appendChild(tooLow);
-  }
+   tips.appendChild(tooHigh);
+
+ } else if ( guess < randomNumberArray.slice(-1)[0]) {
+
+   var tooLow = document.createElement('li');
+
+tooLow.appendChild(document.createTextNode("Nope, " + guess + "'s too low. Try Again!"));
+
+   tips.appendChild(tooLow);
+ }
 }
 
-function resetGame() {
+function resetGame(){
+
+  randomNumberArray.push(Math.floor(Math.random() * 50) + 1);
+  
+
   document.getElementById('submit').disabled = false;
+
   tips.innerHTML = "";
 }
